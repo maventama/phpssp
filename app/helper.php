1ef73311c86f5b77d4501d7ef9a041eea0b533f4
Yoga Bagas <yogabagas69@gmail.com>
2024-11-13 14:01:43 +0700

    Helper csrf token

--- app/helper.php
+++ app/helper.php
@@ -1,7 +1,12 @@
 <?php
 // app/helper.php
 
+require_once __DIR__ . '/Library/CSRF.php';
+
 // Escape output untuk menghindari XSS
 function e($string) {
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+}
+function csrf_token() {
+    return CSRF::generateToken();
 }
